9f5bc2df6cba169d84e4952c4467dca698cb6078
David Jennes <david.jennes@appwise.be>
2017-01-14 18:35:32 +0100

    Storyboards macOS tests

---
+++ Tests/TestSuites/StoryboardsMacOSTests.swift
@@ -0,0 +1,23 @@
+//
+// SwiftGen
+// Copyright (c) 2015 Olivier Halligon
+// MIT Licence
+//
+
+import XCTest
+
+class StoryboardsMacOSTests: XCTestCase {
+  static let contextNames = ["empty", "all", "customname"]
+  
+  func testDefault() {
+    test(template: "storyboards-osx-default", contextNames: StoryboardsMacOSTests.contextNames, outputPrefix: "default", directory: .storyboardsMacOS)
+  }
+  
+  func testSwift3() {
+    test(template: "storyboards-osx-swift3", contextNames: StoryboardsMacOSTests.contextNames, outputPrefix: "swift3", directory: .storyboardsMacOS)
+  }
+  
+  func testLowercase() {
+    test(template: "storyboards-osx-lowercase", contextNames: StoryboardsMacOSTests.contextNames, outputPrefix: "lowercase", directory: .storyboardsMacOS)
+  }
+}
