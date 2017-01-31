2169c9c29cce98fe92c411410be5da368af3f0f1
David Jennes <david.jennes@appwise.be>
2017-01-31 09:46:35 +0100

    fix module compilation

--- Scripts/compile-modules.sh
+++ Scripts/compile-modules.sh
@@ -4,9 +4,10 @@ SWIFT3_TOOLCHAIN=""
 # compile macos modules
 SDK=$(xcrun --show-sdk-path --sdk macosx)
 TARGET="x86_64-apple-macosx10.12"
-MODULES=("PrefsWindowController")
+MODULES=( "PrefsWindowController" )
 
-for m in $MODULES; do
+for m in ${MODULES[@]}; do
+	echo "Compiling module ${m}… (macos)"
 	xcrun $SWIFT3_TOOLCHAIN swiftc -emit-module "Scripts/Modules/$m.swift" -sdk $SDK -module-name "$m" -emit-module-path "Scripts/Modules/swift3" -target $TARGET
 	xcrun $SWIFT2_TOOLCHAIN swiftc -emit-module "Scripts/Modules/$m.swift" -sdk $SDK -module-name "$m" -emit-module-path "Scripts/Modules/swift2.3" -target $TARGET
 done
@@ -14,9 +15,10 @@ done
 #compile ios modules
 SDK=$(xcrun --show-sdk-path --sdk iphoneos)
 TARGET="armv7s-apple-ios10.0"
-MODULES=("CustomSegue" "LocationPicker" "SlackTextViewController")
+MODULES=( "CustomSegue" "LocationPicker" "SlackTextViewController" )
 
-for m in $MODULES; do
+for m in ${MODULES[@]}; do
+	echo "Compiling module ${m}… (ios)"
 	xcrun $SWIFT3_TOOLCHAIN swiftc -emit-module "Scripts/Modules/$m.swift" -sdk $SDK -module-name "$m" -emit-module-path "Scripts/Modules/swift3" -target $TARGET
 	xcrun $SWIFT2_TOOLCHAIN swiftc -emit-module "Scripts/Modules/$m.swift" -sdk $SDK -module-name "$m" -emit-module-path "Scripts/Modules/swift2.3" -target $TARGET
 done
