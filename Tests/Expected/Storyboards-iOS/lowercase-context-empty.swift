e41cd3aeff8b312a325f68173a867f44b2bbde27
David Jennes <david.jennes@gmail.com>
2017-02-13 13:23:03 +0100

    storyboards: fix compilation error for empty contexts

--- Tests/Expected/Storyboards-iOS/lowercase-context-empty.swift
+++ Tests/Expected/Storyboards-iOS/lowercase-context-empty.swift
@@ -3,42 +3,4 @@
 import Foundation
 import UIKit
 
-// swiftlint:disable file_length
-// swiftlint:disable line_length
-// swiftlint:disable type_body_length
-
-protocol StoryboardSceneType {
-  static var storyboardName: String { get }
-}
-
-extension StoryboardSceneType {
-  static func storyboard() -> UIStoryboard {
-    return UIStoryboard(name: self.storyboardName, bundle: NSBundle(forClass: BundleToken.self))
-  }
-
-  static func initialViewController() -> UIViewController {
-    guard let vc = storyboard().instantiateInitialViewController() else {
-      fatalError("Failed to instantiate initialViewController for \(self.storyboardName)")
-    }
-    return vc
-  }
-}
-
-extension StoryboardSceneType where Self: RawRepresentable, Self.RawValue == String {
-  func viewController() -> UIViewController {
-    return Self.storyboard().instantiateViewControllerWithIdentifier(self.rawValue)
-  }
-  static func viewController(identifier: Self) -> UIViewController {
-    return identifier.viewController()
-  }
-}
-
-protocol StoryboardSegueType: RawRepresentable { }
-
-extension UIViewController {
-  func performSegue<S: StoryboardSegueType where S.RawValue == String>(segue: S, sender: AnyObject? = nil) {
-    performSegueWithIdentifier(segue.rawValue, sender: sender)
-  }
-}
-
 // No storyboard found
--- Tests/Expected/Storyboards-macOS/swift3-context-empty.swift
+++ Tests/Expected/Storyboards-macOS/swift3-context-empty.swift
@@ -3,49 +3,4 @@
 import Foundation
 import Cocoa
 
-// swiftlint:disable file_length
-// swiftlint:disable line_length
-// swiftlint:disable type_body_length
-
-protocol StoryboardSceneType {
-  static var storyboardName: String { get }
-}
-
-extension StoryboardSceneType {
-  static func storyboard() -> NSStoryboard {
-    return NSStoryboard(name: self.storyboardName, bundle: Bundle(for: BundleToken.self))
-  }
-
-  static func initialController() -> Any {
-    guard let controller = storyboard().instantiateInitialController()
-    else {
-      fatalError("Failed to instantiate initialViewController for \(self.storyboardName)")
-    }
-    return controller
-  }
-}
-
-extension StoryboardSceneType where Self: RawRepresentable, Self.RawValue == String {
-  func controller() -> Any {
-    return Self.storyboard().instantiateController(withIdentifier: self.rawValue)
-  }
-  static func controller(identifier: Self) -> Any {
-    return identifier.controller()
-  }
-}
-
-protocol StoryboardSegueType: RawRepresentable { }
-
-extension NSWindowController {
-  func performSegue<S: StoryboardSegueType>(segue: S, sender: Any? = nil) where S.RawValue == String {
-    performSegue(withIdentifier: segue.rawValue, sender: sender)
-  }
-}
-
-extension NSViewController {
-  func performSegue<S: StoryboardSegueType>(segue: S, sender: Any? = nil) where S.RawValue == String {
-    performSegue(withIdentifier: segue.rawValue, sender: sender)
-  }
-}
-
 // No storyboard found
