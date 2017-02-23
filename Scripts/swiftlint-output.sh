365e10a4bbdc685d730b80a813dfb6711c81a72a
David Jennes <david.jennes@gmail.com>
2017-02-23 16:47:32 +0100

    lint: fail on warnings

--- Scripts/swiftlint-output.sh
+++ Scripts/swiftlint-output.sh
@@ -1,6 +1,6 @@
 if which swiftlint >/dev/null; then
   # Lint generated code in Tests/Expected
-  swiftlint lint --no-cache --path "Tests/Expected"
+  swiftlint lint --no-cache --strict --path "Tests/Expected"
 else
   echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
 fi
