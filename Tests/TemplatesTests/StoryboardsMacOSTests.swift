4e5a7178fbe5c7fd64c7dc9d6909f883e5d3def7
David Jennes <david.jennes@gmail.com>
2017-03-25 18:07:31 +0100

    Adapt macos storyboards tests to use context generation

--- Tests/TemplatesTests/StoryboardsMacOSTests.swift
+++ Tests/TemplatesTests/StoryboardsMacOSTests.swift
@@ -4,31 +4,49 @@
 // MIT Licence
 //
 
+import StencilSwiftKit
 import XCTest
 
 class StoryboardsMacOSTests: XCTestCase {
   enum Contexts {
-    static let all = ["empty", "all", "customname"]
+    static let all = ["empty", "all"]
+  }
+
+  // generate variations to test customname generation
+  let variations: VariationGenerator = { name, context in
+    guard name == "all" else { return [(context: context, suffix: "")] }
+
+    return [
+      (context: context,
+       suffix: ""),
+      (context: try StencilContext.enrich(context: context,
+                                          parameters: ["sceneEnumName=XCTStoryboardsScene",
+                                                       "segueEnumName=XCTStoryboardsSegue"]),
+       suffix: "-customname")
+    ]
   }
 
   func testDefault() {
     test(template: "storyboards-osx-default",
          contextNames: Contexts.all,
          outputPrefix: "default",
-         directory: .storyboardsMacOS)
+         directory: .storyboardsMacOS,
+         contextVariations: variations)
   }
 
   func testSwift3() {
     test(template: "storyboards-osx-swift3",
          contextNames: Contexts.all,
          outputPrefix: "swift3",
-         directory: .storyboardsMacOS)
+         directory: .storyboardsMacOS,
+         contextVariations: variations)
   }
 
   func testLowercase() {
     test(template: "storyboards-osx-lowercase",
          contextNames: Contexts.all,
          outputPrefix: "lowercase",
-         directory: .storyboardsMacOS)
+         directory: .storyboardsMacOS,
+         contextVariations: variations)
   }
 }
