b7df129d674629ad4ede8ee844df03be3135d7e8
David Jennes <david.jennes@gmail.com>
2017-03-25 17:51:44 +0100

    Adapt font tests to use context variations

--- Tests/TemplatesTests/FontsTests.swift
+++ Tests/TemplatesTests/FontsTests.swift
@@ -6,24 +6,44 @@
 //  Copyright © 2016 AliSoftware. All rights reserved.
 //
 
+import StencilSwiftKit
 import XCTest
 
 class FontsTests: XCTestCase {
   enum Contexts {
-    static let all = ["empty", "defaults", "customname"]
+    static let all = ["empty", "defaults"]
+  }
+
+  // generate variations to test customname generation
+  let variations: VariationGenerator = { name, context in
+    guard name == "defaults" else { return [(context: context, suffix: "")] }
+
+    do {
+      return [
+        (context: context,
+         suffix: ""),
+        (context: try StencilContext.enrich(context: context,
+                                            parameters: ["enumName=CustomFamily"]),
+         suffix: "-customname")
+      ]
+    } catch {
+      fatalError("Unable to create context variations")
+    }
   }
 
   func testDefault() {
     test(template: "fonts-default",
          contextNames: Contexts.all,
          outputPrefix: "default",
-         directory: .fonts)
+         directory: .fonts,
+         contextVariations: variations)
   }
 
   func testSwift3() {
     test(template: "fonts-swift3",
          contextNames: Contexts.all,
          outputPrefix: "swift3",
-         directory: .fonts)
+         directory: .fonts,
+         contextVariations: variations)
   }
 }
